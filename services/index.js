a9da6233f5b96e88b761a8c8703ee0b990c251d4
Bernie Green <26454402+bgreen280@users.noreply.github.com>
2024-07-03 21:36:54 -0500

    update supported services list

--- services/index.js
+++ services/index.js
@@ -1,8 +1,6 @@
 module.exports = {
-  Clickup: require("./clickup"),
   GoogleFi: require("./google-fi"),
   GoogleFit: require("./google-fit"),
   Raindrop: require("./raindrop"),
-  Reddit: require("./reddit"),
   Youtube: require("./youtube"),
 };

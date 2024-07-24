7458f734299666f4e3241807fad65efd4a197098
Bernie Green <26454402+bgreen280@users.noreply.github.com>
2024-07-23 21:56:37 -0500

    resolve linting errors

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+// .eslintrc.cjs
+const path = require('path');
+
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: ['./packages/*/tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  env: {
+    node: true,
+    jest: true,
+    es2021: true,
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+  },
+  overrides: [
+    {
+      files: ['jest.config.ts'],
+      parserOptions: {
+        project: ['./tsconfig.base.json'],
+      },
+    },
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-floating-promises': 'error',
+    eqeqeq: ['error', 'always'],
+    curly: ['error', 'all'],
+  },
+};

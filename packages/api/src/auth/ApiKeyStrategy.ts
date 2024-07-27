429e641dbbaeabb92ecd6567136da1433ef50c69
Bernie Green <26454402+bgreen280@users.noreply.github.com>
2024-07-27 00:34:02 -0500

    minor cleaning

--- packages/api/src/auth/ApiKeyStrategy.ts
+++
@@ -1,9 +0,0 @@
-import { IAuthStrategy, IApiKeyConfig } from '../types.js';
-
-export class ApiKeyStrategy implements IAuthStrategy {
-  constructor(private config: IApiKeyConfig) {}
-
-  async getAuthHeader(): Promise<Record<string, string>> {
-    return { Authorization: `Bearer ${this.config.apiKey}` };
-  }
-}
